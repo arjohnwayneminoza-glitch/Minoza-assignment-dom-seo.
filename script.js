f8a8814daa401b2a41c5b79fca43d0f9f5a890d2
arjohnwayneminoza-glitch <arjohnwayne.minoza@hcdc.edu.ph>
2025-09-06 11:20:37 +0800

    Minoza-assignment-dom-seo.

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("btnMessage").addEventListener("click", function () {
+  document.getElementById("outputText").textContent =
+    "Keep strumming  — music is the language of the soul!";
+});
